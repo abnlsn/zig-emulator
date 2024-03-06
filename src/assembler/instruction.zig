0b8d4617bd575c03313a221229db5a3c2ff9b03c
Abe Nelson <29802261+abnlsn@users.noreply.github.com>
2024-03-06 11:27:56 -0500

    Updated instruction code to make more sense

--- src/assembler/instruction.zig
+++ src/assembler/instruction.zig
@@ -1,101 +1,20 @@
 const std = @import("std");
 const meta = std.meta;
 const modes = @import("cpu").modes;
+pub const Mode = modes.Mode;
 
-const instructionModes = .{modes.immediate.ImmediateOp, modes.arithmetic.ArithmeticOp, modes.memory.MemoryOp, modes.stack.StackOp};
+const Arithmetic = modes.arithmetic.ArithmeticOp;
+const Immediate = modes.immediate.ImmediateOp;
+const Stack = modes.stack.StackOp;
+const Memory = modes.memory.MemoryOp;
 
-pub const AnyInstruction = blk: {
-    var size = 0;
-    for (instructionModes) |mode| {
-        size += meta.fields(mode).len;
-    }
+pub const instructionMap = std.ComptimeStringMapWithEql(modes.Mode, .{
+    .{ "ADD", .{.Arithmetic = .ADD}},
+    .{ "SUB", .{.Arithmetic = .SUB}},
+}, std.comptime_string_map.eqlAsciiIgnoreCase
+);
 
-    var index = 0;
-    var fields: [size]std.builtin.Type.EnumField = undefined;
-
-    for (instructionModes, 0..) |mode, i| {
-        const modeBits = @as(u8, i) << 6;
-        for (meta.fields(mode)) |field| {
-            fields[index] = .{.name = field.name, .value = modeBits | field.value};
-            index += 1;
-        }
-    }
-
-    const enumInfo = std.builtin.Type.Enum {
-        .tag_type = u8,
-        .fields = &fields,
-        .is_exhaustive = true,
-        .decls = &[0]std.builtin.Type.Declaration{},
-    };
-
-    break :blk @Type(std.builtin.Type{ .Enum  = enumInfo });
-};
-
-fn stringToMode(instr: []const u8) !modes.Mode {
-    const operation = std.meta.stringToEnum(AnyInstruction, instr) orelse return error.InvalidInstruction;
-    const mode = (@intFromEnum(operation) >> 6) & 0b11;
-    const opcode = @intFromEnum(operation) & 0b00111111;
-
-    // const Mode = modes.Mode;
-    const ImmediateOp = modes.immediate.ImmediateOp;
-    const ArithmeticOp = modes.arithmetic.ArithmeticOp;
-    const MemoryOp = modes.memory.MemoryOp;
-    const StackOp = modes.stack.StackOp;
-
-    switch (mode) {
-        0 => {
-            const op: ImmediateOp = @enumFromInt(opcode);
-            return .{ .Immediate = op };
-        },
-        1 => {
-            const op: ArithmeticOp = @enumFromInt(opcode);
-            return .{ .Arithmetic = op };
-        },
-        2 => {
-            const op: MemoryOp = @enumFromInt(opcode);
-            return .{ .Memory = op};
-        },
-        3 => {
-            const op: StackOp = @enumFromInt(opcode);
-            return .{ .Stack = op };
-        },
-        else => {
-            return error.InvalidMode;
-        },
-    }
-    
-}
-
-fn constructOpcode(instr: []const u8) !u8 {
-    const operation = try stringToMode(instr);
-    const keep = 0b0;
-    const wst: u8 = 0b0;
-
-    switch (operation) {
-        .Immediate => {
-            const op = operation.Immediate;
-            return (keep << 7) | (wst << 6) | 0b00 << 4 | @intFromEnum(op);
-        },
-        .Arithmetic => {
-            const op = operation.Arithmetic;
-            return (keep << 7) | (wst << 6) | 0b01 << 4 | @intFromEnum(op);
-        },
-        .Stack => {
-            const op = operation.Stack;
-            const dst: u8 = 0b0;
-            const pos: u8 = 0b00;
-            return (keep << 7) | (wst << 6) | 0b11 << 4 | dst << 3 | pos << 1 | @intFromEnum(op);
-        },
-        .Memory => {
-            const op = operation.Memory;
-            const page: u8 = 0b00;
-            return (keep << 7) | (wst << 6) | 0b11 << 4 | page << 2 | @intFromEnum(op);
-        },
-    }
-}
-
-test "constructOpcode" {
-    const instr = "ADD";
-    const opcode = try constructOpcode(instr);
-    try std.testing.expect(opcode == 0b00010100);
+test "instruction map ADD" {
+    try std.testing.expectEqual(instructionMap.get("add"), Mode{.Arithmetic = .ADD});
+    try std.testing.expectEqual(instructionMap.get("ADD"), Mode{.Arithmetic = .ADD});
 }
